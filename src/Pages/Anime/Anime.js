03a51132981e72e59317405e9d75e7ddf0264a49
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2023-04-13 18:19:29 +0100

    a start on the Anime Page

--- src/Pages/Anime/Anime.js
+++ src/Pages/Anime/Anime.js
@@ -3,6 +3,7 @@ import { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import ThemeContext from "../../Components/Contexts/ThemeContext";
 import "./Anime.css"
+import SteinsGate from "../../Assets/Image buttons/SteinsGate.png"
 
 function Show() {
 
@@ -16,14 +17,20 @@ function Show() {
   }, [isLeftMode]);
 
   return (
-    <div className="container">
-<div className="box">
-    <div class="textContainer">
-      <br /> <br />
-      <div class="title">
-        <span class="block-title"></span>
-        <h1 class="firstText">Top 10 anime</h1>
+  <div className="container">
+    <div className="box">
+      <div class="textContainer">
+        <br /> <br />
+        <div class="title">
+          <span class="block-title"></span>
+          <h1 class="firstText">Top 10 anime</h1>
+        </div>
       </div>
+      <div className="Anime" >
+      <Link to="/Anime" src={SteinsGate} alt="steinsGate" name="#1">SteinsGate</Link>
+      <br />
+      <Link to="/Anime" src="" alt="BloodBlockateBattlefront" name="#2">Kekkai Sensen</Link>
+      <br />
     </div>
   </div>
 
