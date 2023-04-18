abc3df7f2b3ad77aea53671abaf4d14d3047e05a
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2023-04-18 02:00:32 +0100

    updated half of the given images and added them to the buttons

--- PersonalSite/src/Pages/Anime2/Anime2.jsx
+++ PersonalSite/src/Pages/Anime2/Anime2.jsx
@@ -8,7 +8,10 @@ import SteinsGate from "../../Assets/Image buttons/SteinsGate.png"
 import kekkeisensen from "../../Assets/Image buttons/bloodblockadebattlefront.png"
 import steins from "../../Assets/Image buttons/stiensgateclocks.png"
 import aot from "../../Assets/Image buttons/attackontitanbg.png"
-
+import lupinIII from "../../Assets/Image buttons/lupin.png"
+import onePunch from "../../Assets/Image buttons/onepunchmanbg.png"
+import nichijou from "../../Assets/Image buttons/nichijoutitle.png"
+import ghoul from "../../Assets/Image buttons/tokyo-ghoulbg.png"
 function Anime() {
 
   const {isLeftMode} = useContext(ThemeContext);
@@ -38,14 +41,13 @@ function Anime() {
             </Link>
             <Link to="/Anime" alt="BloodBlockateBattlefront" name="#2" className="two" style={{backgroundImage: `url(${kekkeisensen})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="BloodBlockateBattlefront" name="#3" className="three" style={{backgroundImage: `url(${aot})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link to="/Anime" alt="Attack on Titan" name="#3" className="three" style={{backgroundImage: `url(${aot})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="BloodBlockateBattlefront" name="#4" className="four" style={{backgroundImage: `url(${steins})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link to="/Anime" alt="Tokyo Ghoul" name="#4" className="four" style={{backgroundImage: `url(${ghoul})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="BloodBlockateBattlefront" name="#5" className="five" style={{backgroundImage: `url(${steins})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link to="/Anime" alt="One punch man" name="#5" className="five" style={{backgroundImage: `url(${onePunch})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="BloodBlockateBattlefront" name="#6" className="six" style={{backgroundImage: `url(${steins})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
-              Kekkai Sensen
+            <Link to="/Anime" alt="lupin the third" name="#6" className="six" style={{backgroundImage: `url(${lupinIII}})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
             <Link to="/Anime" alt="BloodBlockateBattlefront" name="#7" className="seven" style={{backgroundImage: `url(${steins})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
               Kekkai Sensen
@@ -56,7 +58,7 @@ function Anime() {
             <Link to="/Anime" alt="BloodBlockateBattlefront" name="#9" className="nine" style={{backgroundImage: `url(${steins})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
               Kekkai Sensen
             </Link>
-            <Link to="/Anime" alt="BloodBlockateBattlefront" name="#10" className="ten" style={{backgroundImage: `url(${steins})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link to="/Anime" alt="BloodBlockateBattlefront" name="#10" className="ten" style={{backgroundImage: `url(${nichijou})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
               Kekkai Sensen
             </Link>
           </div>
