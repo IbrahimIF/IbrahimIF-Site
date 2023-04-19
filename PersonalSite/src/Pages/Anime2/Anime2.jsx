e6b342ca4261d14384c3b876816175b1bb3267b7
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2023-04-19 17:18:17 +0100

    added alll of the audios for the 5th page and swapped some images out

--- PersonalSite/src/Pages/Anime2/Anime2.jsx
+++ PersonalSite/src/Pages/Anime2/Anime2.jsx
@@ -18,11 +18,39 @@ import stone from "../../Assets/Image buttons/dr-stonebg.png"
 import jujutsu from "../../Assets/Image buttons/Jujutsu-Kisenbg.png"
 import slayer from "../../Assets/Image buttons/demon-slayer.png"
 
+import slayerAudio from "../../Assets/Audio/Demon Slayer.mp3"
+import steinsAudio from "../../Assets/Audio/Steins;Gate.mp3"
+import aotAudio from "../../Assets/Audio/Attack on titan.mp3"
+import onepunchAudio from "../../Assets/Audio/One Punch Man.mp3"
+import nichijouAuido from "../../Assets/Audio/Nichijou.mp3"
+import onepieceAudio from "../../Assets/Audio/One Piece.mp3"
+import drstoneAudio from "../../Assets/Audio/Dr Stone.mp3"
+import fmabAudio from "../../Assets/Audio/Fmab.mp3"
+import noteAudio from "../../Assets/Audio/Death Note.mp3"
+import jojoAudio from "../../Assets/Audio/JoJo's bizzare adventure.mp3"
+import bbbfAudio from "../../Assets/Audio/Kekkai Sensen.mp3"
+import tokyoAudio from "../../Assets/Audio/Tokyo Ghoul.mp3"
+import JujutsuAudio from "../../Assets/Audio/Jujutsu Kaisen.mp3"
+
 function Anime() {
 
   const [showExplanation, setShowExplanation] = useState("");
 
   const {isLeftMode} = useContext(ThemeContext);
+
+  var slayers = document.getElementById("slayers2");
+  var stones = document.getElementById("stones2");
+  var jujutsus = document.getElementById("jujutsus2");
+  var sensens = document.getElementById("sensens2");
+  var tokyos = document.getElementById("tokyos2");
+  var jojos = document.getElementById("jojos2");
+  var steinss = document.getElementById("steinss2");
+  var onepieces = document.getElementById("onepieces2");
+  var onepunchs = document.getElementById("onepunchs2");
+  var nichijous = document.getElementById("nichihjous2");
+  var metals = document.getElementById("metals2");
+  var notes = document.getElementById("notes2");
+  var Attacks = document.getElementById("Attacks2");
 
 
   useEffect(() => {
@@ -48,6 +76,48 @@ function Anime() {
 
   return (
     <div className="container">
+      <audio id="slayers2">
+  <source src={slayerAudio} type="audio/mpeg"></source>
+</audio>
+<audio id="tokyos2">
+  <source src={tokyoAudio} type="audio/mpeg"></source>
+</audio>
+<audio id="jujutsus2">
+  <source src={JujutsuAudio} type="audio/mpeg"></source>
+</audio>
+<audio id="jojos2">
+  <source src={jojoAudio} type="audio/mpeg"></source>
+</audio>
+<audio id="slayers2">
+  <source src={steinsAudio} type="audio/mpeg"></source>
+</audio>
+<audio id="onpieces2">
+  <source src={onepieceAudio} type="audio/mpeg"></source>
+</audio>
+<audio id="onepunchs2">
+  <source src={onepunchAudio} type="audio/mpeg"></source>
+</audio>
+<audio id="nichijous2">
+  <source src={nichijouAuido} type="audio/mpeg"></source>
+</audio>
+<audio id="metals2">
+  <source src={fmabAudio} type="audio/mpeg"></source>
+</audio>
+<audio id="notes2">
+  <source src={noteAudio} type="audio/mpeg"></source>
+</audio>
+<audio id="Attacks2">
+  <source src={aotAudio} type="audio/mpeg"></source>
+</audio>
+<audio id="stones2">
+  <source src={drstoneAudio} type="audio/mpeg"></source>
+</audio>
+<audio id="sensens2">
+  <source src={bbbfAudio} type="audio/mpeg"></source>
+</audio>
+<audio id="steinss2">
+  <source src={steinsAudio} type="audio/mpeg"></source>
+</audio>
       <div className="box2">
         <div className="Title">
           <div className="textContainer2">
@@ -70,46 +140,134 @@ function Anime() {
         <div className="top-10">
           <div className="Anime">
             <Link onMouseOver={() => {
+              steinss.play();
                 setShowExplanation("#1");
-                /*tests.start()*/
               }}
               onMouseOut={() => {
-                  setShowExplanation("");
-                /*tests.pause();*/
+                steinss.pause();
+                  setShowExplanation(".");
               }} 
             to="/Anime" alt="Steins Gate" name="#1" className="one" style={{backgroundImage: `url(${steins})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
             <Link  onMouseOver={() => {
+              sensens.play();
                 setShowExplanation("#2");
-                /*tests.start()*/
               }}
               onMouseOut={() => {
-                setShowExplanation("");
-                /*tests.pause();*/
+                sensens.pause();
+                setShowExplanation(".");
               }} 
             to="/Anime" alt="BloodBlockateBattlefront" name="#2" className="two" style={{backgroundImage: `url(${kekkeisensen})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="Attack on Titan" name="#3" className="three" style={{backgroundImage: `url(${aot})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link onMouseOver={() => {
+              Attacks.play();
+                setShowExplanation("#3");
+              }}
+              onMouseOut={() => {
+                Attacks.pause();
+                setShowExplanation(".");
+              }} 
+            to="/Anime" alt="Attack on Titan" name="#3" className="three" style={{backgroundImage: `url(${aot})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="Tokyo Ghoul" name="#4" className="four" style={{backgroundImage: `url(${ghoul})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link onMouseOver={() => {
+              tokyos.play();
+                setShowExplanation("#3");
+              }}
+              onMouseOut={() => {
+                tokyos.pause();
+                setShowExplanation(".");
+              }} 
+            to="/Anime" alt="Tokyo Ghoul" name="#4" className="four" style={{backgroundImage: `url(${ghoul})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="One punch man" name="#5" className="five" style={{backgroundImage: `url(${onePunch})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link onMouseOver={() => {
+              onepunchs.play();
+                setShowExplanation("#3");
+              }}
+              onMouseOut={() => {
+                onepunchs.pause();
+                setShowExplanation(".");
+              }} 
+            to="/Anime" alt="One punch man" name="#5" className="five" style={{backgroundImage: `url(${onePunch})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="jojo" name="#6" className="six" style={{backgroundImage: `url(${jojo})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link onMouseOver={() => {
+              jojos.play();
+                setShowExplanation("#3");
+              }}
+              onMouseOut={() => {
+                jojos.pause();
+                setShowExplanation(".");
+              }} 
+            to="/Anime" alt="jojo" name="#6" className="six" style={{backgroundImage: `url(${jojo})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="onepiece" name="#7" className="seven" style={{backgroundImage: `url(${onepiece})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link onMouseOver={() => {
+              onepieces.play();
+                setShowExplanation("#3");
+              }}
+              onMouseOut={() => {
+                onepieces.pause();
+                setShowExplanation(".");
+              }} 
+            to="/Anime" alt="onepiece" name="#7" className="seven" style={{backgroundImage: `url(${onepiece})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="Fullmetal aclhemist" name="#8" className="eight" style={{backgroundImage: `url(${fmab})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link onMouseOver={() => {
+              metals.play();
+                setShowExplanation("#3");
+              }}
+              onMouseOut={() => {
+                metals.pause();
+                setShowExplanation(".");
+              }} 
+            to="/Anime" alt="Fullmetal aclhemist" name="#8" className="eight" style={{backgroundImage: `url(${fmab})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="DeathNote" name="#9" className="nine" style={{backgroundImage: `url(${note})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link onMouseOver={() => {
+              notes.play();
+                setShowExplanation("#3");
+              }}
+              onMouseOut={() => {
+                notes.pause();
+                setShowExplanation(".");
+              }} 
+             to="/Anime" alt="DeathNote" name="#9" className="nine" style={{backgroundImage: `url(${note})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="Dr Stone" name="#10" className="ten" style={{backgroundImage: `url(${stone})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link onMouseOver={() => {
+              stones.play();
+                setShowExplanation("#3");
+              }}
+              onMouseOut={() => {
+                stones.pause();
+                setShowExplanation(".");
+              }} 
+             to="/Anime" alt="Dr Stone" name="#10" className="ten" style={{backgroundImage: `url(${stone})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="nichijou" name="#11" className="eleven" style={{backgroundImage: `url(${nichijou})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link onMouseOver={() => {
+              nichijous.play();
+                setShowExplanation("#3");
+              }}
+              onMouseOut={() => {
+                nichijous.pause();
+                setShowExplanation(".");
+              }} 
+            o="/Anime" alt="nichijou" name="#11" className="eleven" style={{backgroundImage: `url(${nichijou})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="JujutsuKisen" name="#12" className="twelve" style={{backgroundImage: `url(${jujutsu})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link onMouseOver={() => {
+              jujutsus.play();
+                setShowExplanation("#3");
+              }}
+              onMouseOut={() => {
+                jujutsus.pause();
+                setShowExplanation(".");
+              }}  
+            to="/Anime" alt="JujutsuKisen" name="#12" className="twelve" style={{backgroundImage: `url(${jujutsu})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
-            <Link to="/Anime" alt="demonslayer" name="#13" className="thirteen" style={{backgroundImage: `url(${slayer})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
+            <Link onMouseOver={() => {
+              slayers.play();
+                setShowExplanation("#3");
+              }}
+              onMouseOut={() => {
+                slayers.pause();
+                setShowExplanation(".");
+              }} 
+              to="/Anime" alt="demonslayer" name="#13" className="thirteen" style={{backgroundImage: `url(${slayer})`, borderRadius: "0.375rem", backgroundSize: "cover", height: "100px", zIndex: "5"}}>
             </Link>
           </div>
         </div>
